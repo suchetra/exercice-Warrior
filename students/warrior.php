f9fcac366b9b489b5b9a5e1378e3a3013cf3abe7
suchetra <nicolas.tan@le-campus-numerique.fr>
2021-08-27 12:01:24 +0200

    étape 9 en cours

--- students/warrior.php
+++ students/warrior.php
@@ -3,9 +3,97 @@
 require_once __DIR__ . "/../base/localWarrior.php";
 
 
-$GLOBALS['warriorID'] = 'azertyuiop';
+// 1/ Modifiez la variable globale warriorID dans le fichier students/warrior.php
+$GLOBALS['warriorID'] = 'Nicolas';
 
 // Définissez vos class de combattants
 abstract class Warrior extends LocalWarrior {
 
+    
+    // 4/ La classe Warrior doit avoir les attributs (publics) $id, $name, $speed, $life, $shield, $imageUrl et $weapon
+    public $id;
+    public $name;
+    public $speed;
+    public $life;
+    public $shield;
+    public $imageUrl;
+    public $weapon;
+
+    
+    // 7/ Les classes Warrior, StartrekWarrior, MarvelWarrior et PokemonWarrior doivent avoir des constructeurs
+    public function __construct($id){
+        
+        
+        // 8/ Le constructeur de Warrior doit prendre en paramètre un $id et initialiser l'id, speed=30, life=100, shield=20
+        $this->id = $id;
+        $this->speed = 30;
+        $this->life = 100;
+        $this->shield = 20;
+
+        // $id = $id;
+        // $speed = 30;
+        // $life = 100;
+        // $shield = 20;
+    }
+
 };
+
+// 2/ Les classes StartrekWarrior, MarvelWarrior et PokemonWarrior doivent être créées dans le ficher students/warrior.php
+class StartrekWarrior extends Warrior {
+
+    // 5/ Les classe StartrekWarrior, MarvelWarrior et PokemonWarrior doivent avoir respectivement les attributs $mentalPower, $superPower et $level
+    public $mentalPower;
+
+    public function power() {
+        // 6/Les classes StartrekWarrior, MarvelWarrior et PokemonWarrior doivent avoir une méthode power qui retournent respectivement $mentalPower, $superPower et $level
+        return $this->mentalPower;
+
+    }
+
+    // 7/ Les classes Warrior, StartrekWarrior, MarvelWarrior et PokemonWarrior doivent avoir des constructeurs
+    public function __construct($mentalPower){
+        parent:: __construct($mentalPower){
+        }
+        $this->mentalPower = 8;
+    }
+
+}
+
+class MarvelWarrior extends Warrior {
+
+    // 5/ Les classe StartrekWarrior, MarvelWarrior et PokemonWarrior doivent avoir respectivement les attributs $mentalPower, $superPower et $level
+    public $superPower;
+
+    public function power() {
+        // 6/Les classes StartrekWarrior, MarvelWarrior et PokemonWarrior doivent avoir une méthode power qui retournent respectivement $mentalPower, $superPower et $level
+        return $this->superPower;
+    }
+
+    // 7/ Les classes Warrior, StartrekWarrior, MarvelWarrior et PokemonWarrior doivent avoir des constructeurs
+    public function __construct(){
+        // parent:: __construct(){
+        //     $this->superPower = 100;
+        // }
+    }
+
+}
+
+class PokemonWarrior extends Warrior {
+
+    // 5/ Les classe StartrekWarrior, MarvelWarrior et PokemonWarrior doivent avoir respectivement les attributs $mentalPower, $superPower et $level
+    public $level;
+
+    public function power() {
+        // 6/Les classes StartrekWarrior, MarvelWarrior et PokemonWarrior doivent avoir une méthode power qui retournent respectivement $mentalPower, $superPower et $level
+        return $this->level;
+
+    }
+
+    // 7/ Les classes Warrior, StartrekWarrior, MarvelWarrior et PokemonWarrior doivent avoir des constructeurs
+    public function __construct(){
+        // parent:: __construct(){
+        //     $this->level = 1;
+        // }
+    }
+
+}
